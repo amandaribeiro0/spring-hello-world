2165f68fc2c2ecaf6a6e54be0ff0f9099d32a197
Amanda Ribeiro <aamandaribeirocosta@gmail.com>
2023-04-12 18:31:51 -0300

    Hello World

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -10,12 +10,32 @@ public class HelloController {
 	
 		@GetMapping
 		public String hello() {
-			return "<h3 style=\"color:red;padding:30px;\">Hello Turma 62!</h3>"
-					+ "<br/>"
-					+ "Sou Amandica";
+			return "<h1 style=\"color:purple;text-align:center;\">Hello World!</h1>";
 		}
-		@GetMapping("/nome")
-		public String meunome() {
-			return "<h3>Amandica</h3>";
+		@GetMapping("/bsm")
+		public String bsm() {
+			return "<b style=\"color:purple;font-size:35px;\">BSM’s</b>"
+					+ "<br/>"
+					+ "Mentalidade de crescimento"
+					+ "<br/>"
+					+ "Persistência"
+					+ "<br/>"
+					+ "Responsabilidade pessoal"
+					+ "<br/>"
+					+ "Orientação ao futuro"
+					+ "<br/>"
+					+ "Comunicação"
+					+ "<br/>"
+					+ "Orientação ao detalhe"
+					+ "<br/>"
+					+ "Proatividade"
+					+ "<br/>"
+					+ "Trabalho em equipe";
+		}
+		@GetMapping ("/objetivo")
+		public String objetivo() {
+			return "<b style=\"color:purple;font-size:35px;\">Objetivos de aprendizagem desta semana.</b>"
+					+ "<br/>"
+					+ "Meu objetivo de aprendizagem essa semana é aprender os conceitos basicos de Spring e me aprofundar mais no assunto para desenvolver meu blog pessoal";
 		}
 }
